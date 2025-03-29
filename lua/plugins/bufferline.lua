188852494aa1cfe61d8307da466c1c60b59ad4c8
RevimNeo <neorevim@gmail.com>
2025-03-30 01:55:30 +0800

    better bufferline tab closing

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -10,6 +10,16 @@ return {
 		vim.api.nvim_set_keymap('n', '<Tab>', ':BufferLineCycleNext<CR>', { noremap = true, silent = true })
 		vim.api.nvim_set_keymap('n', '<S-Tab>', ':BufferLineCyclePrev<CR>', { noremap = true, silent = true })
 		vim.api.nvim_set_keymap('n', '<leader>bc', ':BufferLinePickClose<CR>', { noremap = true, silent = true })
-		vim.api.nvim_set_keymap('n', '<leader><Tab>', ':bd<CR>', { noremap = true, silent = true })
+		vim.api.nvim_set_keymap('n', '<leader><Tab>', ':lua CloseBuffer() <CR>', { noremap = true, silent = true })
+
+		function CloseBuffer()
+			local bufs = vim.fn.getbufinfo({ buflisted = 1 }) -- Get list of buffers
+
+			if #bufs == 1 then
+				vim.cmd("quit") -- Quit Neovim if only one buffer left
+			else
+				vim.cmd("bprev | bd #") -- Switch to next buffer, then delete current
+			end
+		end
 	end
 }
